78b8b3ab2b3060d61f5af88f321b625dcb84d5e1
David <kouchdavid78@gmail.com>
2026-06-28 16:02:01 +0700

    feat: update

---
+++ lib/layouts/master_layout.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MasterLayout extends StatefulWidget {
+  const MasterLayout({
+    super.key
+  });
+
+
+  @override
+  State<StatefulWidget> createState() => _MasterLayoutState();
+}
+
+class _MasterLayoutState extends State<MasterLayout> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/screens/unauthorization_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UnauthorizationScreen extends StatefulWidget {
+  const UnauthorizationScreen({super.key});
+  @override
+  State<StatefulWidget> createState() => _UnauthorizationScreenState();
+}
+
+class _UnauthorizationScreenState extends State<UnauthorizationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
